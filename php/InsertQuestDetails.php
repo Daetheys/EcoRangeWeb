5aded7cd89bdeb50ec79b1c22d86a3267f27472f
Daetheys <daetheys@gmail.com>
2022-06-30 19:21:46 +0200

    Added missing files

---
+++ php/InsertQuestDetails.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'connectDB.php';
+
+$EXPID = stripslashes(htmlspecialchars($_POST['expID']));
+$ID = stripslashes(htmlspecialchars($_POST['id']));
+$EXP = stripslashes(htmlspecialchars($_POST['exp']));
+$TEXT = stripslashes(htmlspecialchars($_POST['text'])); 
+
+$stmt = $db->prepare("INSERT INTO nicolas_ecorange_exp VALUE(?,?,?,?,NOW())");
+$stmt->bind_param("ssss",$EXPID,$ID,$EXP,$TEXT);
+$stmt->execute();
+$err = $stmt->errno ;
+$data = array(
+      'error' => $err,
+    );
+$stmt->close();
+ $db->close();
+echo json_encode($data);
+ ?>
