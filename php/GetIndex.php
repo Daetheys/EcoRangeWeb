126e180d2c9360cca06a58199c044e53fe03966f
Daetheys <daetheys@gmail.com>
2022-07-07 15:51:20 +0200

    Patched GetIndex

--- php/GetIndex.php
+++ php/GetIndex.php
@@ -2,10 +2,18 @@
 
 include 'connectDB.php';
 
-$data = $db->query("SELECT ENVID FROM nicolas_ecorange_exp");
+$maxi = 80;
+$val = 0;
+$query= $db->query("SELECT ENVID FROM nicolas_ecorange_exp");
+while($data = $query->fetch_row()){
+    $val = (int)$data[0];
+    if ($val > $maxi){
+       $maxi = $val;
+    }
+}
 
 $db->close();
 
-echo json_encode($data);
+echo json_encode($maxi);
 
 ?>
