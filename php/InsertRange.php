da16d5a41084eac57420d84cc59a7abf03b765d5
Nicolas Yax <daetheys@gmail.com>
2022-10-11 12:33:17 +0200

    Added missing InserRange.php file

---
+++ php/InsertRange.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'connectDB.php';
+
+$EXP 		= stripslashes(htmlspecialchars($_POST['exp']));
+$EXPID 		= stripslashes(htmlspecialchars($_POST['expID']));
+$ID 		= stripslashes(htmlspecialchars($_POST['id']));
+$TEST 		= stripslashes(htmlspecialchars($_POST['test']));
+$SEASON		= stripslashes(htmlspecialchars($_POST['season']));
+$TRIAL 		= stripslashes(htmlspecialchars($_POST['trial']));
+$MIN 	    = stripslashes(htmlspecialchars($_POST['min']));
+$MAX      = stripslashes(htmlspecialchars($_POST['max']));
+$RTIME 		= stripslashes(htmlspecialchars($_POST['reaction_time']));
+$SESSION 	= stripslashes(htmlspecialchars($_POST['session']));
+$CTIME 		= stripslashes(htmlspecialchars($_POST['choice_time']));
+
+$stmt = $db->prepare("INSERT INTO nicolas_ecorange_range VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())");
+$stmt->bind_param("sssiiittdid",
+    $EXP, $EXPID, $ID, $TEST, $SEASON, $TRIAL, $MIN, $MAX, $RTIME, $SESSION, $CTIME
+);
+$stmt->execute();
+$err = $stmt->errno ;
+$data = array(
+      'error' => $err,
+    );
+$stmt->close();	
+ $db->close();		
+echo json_encode($data);
+ ?>
